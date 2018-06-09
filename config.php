1cff24adc7ddfe9c7c61e91140847662a9606754
Highland Pan <402945786@qq.com>
2018-06-10 02:00:54 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'flaa',
+    'username' => 'root',
+    'password' => '1234Abcd',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://xijuzhai.com',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
